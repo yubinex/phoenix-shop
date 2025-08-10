27a5997cb8c504a275a25a4bd156a537b49b2a4b
yubinex <janhauck99@gmail.com>
2025-08-10 16:08:30 +0200

    feat: Update product HTML templates to use new product component and remove redundant headings

--- lib/shop_web/controllers/product_html.ex
+++ lib/shop_web/controllers/product_html.ex
@@ -2,4 +2,12 @@ defmodule ShopWeb.ProductHTML do
   use ShopWeb, :html
 
   embed_templates "product_html/*"
+
+  attr :name, :string, required: true
+
+  def product(assigns) do
+    ~H"""
+    <p>Game: {@name}</p>
+    """
+  end
 end
