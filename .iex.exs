c9b3e4ef7e7dc9b0bc8b7354757407ab26fbd5d0
yubinex <janhauck99@gmail.com>
2025-08-11 18:51:03 +0200

    feat: Add new functions to Products context
    This commit adds the following new functions to the `Products` context:
    
    - `find_product_by_id/1`
    - `create_product/1`
    - `delete_product/1`
    
    These functions provide more complete CRUD functionality for products.

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,4 @@
 alias Shop.Repo
+alias Shop.Products
 alias Shop.Products.Product
 import Ecto.Query
--- lib/shop/products.ex
+++ lib/shop/products.ex
@@ -7,4 +7,18 @@ defmodule Shop.Products do
   def find_product_by_slug(slug) when is_binary(slug) do
     Repo.get_by(Product, slug: slug)
   end
+
+  def find_product_by_id(id) do
+    Repo.get(Product, id)
+  end
+
+  def create_product(attrs \\ %{}) do
+    %Product{}
+    |> Product.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def delete_product(product = %Product{}) do
+    Repo.delete(product)
+  end
 end
